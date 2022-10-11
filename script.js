1405930c4ede51a903764f8944d97bcb9a238691
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-11 13:19:38 +0200

    Finished the responsive navbar

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log(1);
+const hamburgerBtn = document.querySelector('.hamburger-btn');
+const navLinks = document.querySelector('.nav-links');
+
+hamburgerBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
